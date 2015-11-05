2757fdf5414d71b6438753a161e2b2a9329b681e
jieqianzhang <zhang.jieqian@gmail.com>
2015-11-04 20:41:53 -0800

    movefolder

---
+++ depression/js/scroll_content.js
@@ -0,0 +1,63 @@
+jQuery(document).ready(function($){
+        $(".btn").click(function(){
+            $("body").scrollTo($("#location1"),500);
+        });
+         
+        $("#img-one").click(function(){
+            $("body").scrollTo($("#img-small-one"),500);
+        });
+         
+        $("#img-two").click(function(){
+            $("body").scrollTo($("#img-small-two"),500);
+        });
+         
+        $("#img-three").click(function(){
+            $("body").scrollTo($("#img-small-three"),500);
+        });
+         
+        $("#img-four").click(function(){
+            $("body").scrollTo($("#img-small-four"),500);
+        });
+        
+        $("#img-five").click(function(){
+            $("body").scrollTo($("#img-small-five"),500);
+        });
+         
+        $("#img-six").click(function(){
+            $("body").scrollTo($("#img-small-six"),500);
+        });
+         
+        $("#img-seven").click(function(){
+            $("body").scrollTo($("#img-small-seven"),500);
+        });
+         
+        $("#img-eight").click(function(){
+            $("body").scrollTo($("#img-small-eight"),500);
+        });
+         
+        $("#visa").click(function(){
+            $("body").scrollTo($("#visa_explain"),500);
+        });
+         
+        $("#finance").click(function(){
+            $("body").scrollTo($("#finance_explain"),500);
+        });
+         
+        $("#marriage").click(function(){
+            $("body").scrollTo($("#marriage_explain"),500);
+        });
+    
+        $("#culture").click(function(){
+            $("body").scrollTo($("#culture_explain"),500);
+        });
+    
+        $("#language").click(function(){
+            $("body").scrollTo($("#language_explain"),500);
+        });
+    
+        $("#education").click(function(){
+            $("body").scrollTo($("#education_explain"),500);
+        });
+            
+                
+    }); 
