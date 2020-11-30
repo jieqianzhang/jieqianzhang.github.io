526323e40fa2bc2066c7bc55fd4ff0323930bc4d
Jieqian Zhang <jieqian_zhang@apple.com>
2020-11-30 00:31:19 -0500

    clean up code

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,7 @@ var gulp = require('gulp'),
     fs = require('fs'),
     rename = require('gulp-rename');
 
+
 /**
  * Minify and combine JS files, including jQuery and Bootstrap
  */
@@ -19,30 +20,27 @@ const options = {
 
 const data = JSON.parse(fs.readFileSync('src/data/data.json', 'utf8'));
 
-gulp.task('compileIndex', function() {
+
+const compileIndex = (done) => {
     gulp.src(['src/index.hbs'])
         .pipe(handlebars(data, options))
         .pipe(rename('index.html'))
-        .pipe(gulp.dest(''));
-});
+        .pipe(gulp.dest('./'));
+    done();
+};
 
-
-
-gulp.task('scripts', function() {
+const compileScript = (done) => {
     gulp.src([
-            'node_modules/jquery/dist/jquery.js',
             'node_modules/bootstrap/dist/js/bootstrap.js',
             'src/js/**/*.js'
         ])
         .pipe(uglify())
         .pipe(concat('script.js'))
         .pipe(gulp.dest('dist/js'));
-});
+    done();
+}
 
-/**
- * Build SASS, combine with Bootstrap CSS and minify
- */
-gulp.task('sass', function() {
+const compileStyle = (done) => {
     gulp.src([
             'src/sass/main.scss'
         ])
@@ -51,44 +49,64 @@ gulp.task('sass', function() {
         .pipe(minifyCSS())
         .pipe(concat('style.css'))
         .pipe(gulp.dest('dist/css'));
-});
+    done()
+}
 
 /**
  * Move bootstrap and project font files into dist
  */
-gulp.task('fonts', function() {
+const compileFont = (done) => {
     gulp.src([
             'node_modules/bootstrap/dist/fonts/*',
             'src/fonts/*'
         ])
         .pipe(gulp.dest('dist/fonts'));
-});
+    done()
+}
 
 /**
- * The default gulp task
+ * Build individual pages based on the data.json file
  */
-gulp.task('default', function() {
-    gulp.run('scripts', 'sass', 'fonts', 'compileIndex');
-});
+const buildPages = (done) => {
+    const pages = data['projects'].map((d) => {
+        // Right here, we return a function per country
+        return () =>
+            gulp.src(['src/page.hbs'])
+            .pipe(handlebars(d, options))
+            .pipe(rename('index.html'))
+            .pipe(gulp.dest('pages/' + d.slug));
+    });
+
+    return gulp.series(...pages, (seriesDone) => {
+        seriesDone();
+        done();
+    })();
+}
+
+/**
+ * The default gulp task to build the pages
+ */
+gulp.task('default', gulp.series(compileScript, compileStyle, compileFont, compileIndex, buildPages))
+
 
 /**
  * Watch asset files for changes. First runs default to prevent annoying issues.
  */
-gulp.task('watch', function() {
-    gulp.run('default');
+// gulp.task('watch', function() {
+//     gulp.run('default');
 
-    gulp.watch('src/index.hbs', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        gulp.run('compileIndex');
-    });
+//     gulp.watch('src/index.hbs', function(event) {
+//         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+//         gulp.run('compileIndex');
+//     });
 
-    gulp.watch('src/sass/**/*.scss', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        gulp.run('sass');
-    });
+//     gulp.watch('src/sass/**/*.scss', function(event) {
+//         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+//         gulp.run('sass');
+//     });
 
-    gulp.watch('src/js/**/*.js', function(event) {
-        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        gulp.run('scripts');
-    });
-});
+//     gulp.watch('src/js/**/*.js', function(event) {
+//         console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+//         gulp.run('scripts');
+//     });
+// });
